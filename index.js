3f41d92860d67b94cf39b5dfec78f590510f7937
Dan Foy <contact@danfoy.com>
2018-01-05 18:53:12 +0000

    Fix typo in index.js
    `Wlak` to `Walk` on line 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var postcss = require('postcss');
 // PostCSS plugin that lets you write 'nope' instead of 'none'
 module.exports = postcss.plugin('postcss-nope', function () {
     return function (root) {
-        // Wlak each declaration
+        // Walk each declaration
         root.walkDecls(function (decl) {
             // Replace declaration value 'nope' with 'none'
             decl.value === 'nope' && (decl.value = 'none');
